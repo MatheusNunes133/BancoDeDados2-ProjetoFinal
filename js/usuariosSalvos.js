05becfcd3d193e189c8562a30ee3745faedd2a9e
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-03-13 21:20:03 -0300

    feat: Adicionando botoes de editar e excluir

--- js/usuariosSalvos.js
+++ js/usuariosSalvos.js
@@ -38,39 +38,57 @@ async function returnUsers(){
 
 //Função responsável por criar elementos
 function createElements(item){
+    //Div geral
     let div = document.createElement('div')
-            div.classList.add('users')
-        
-        let button = document.createElement('button')
-            button.type = 'button'
-            button.textContent = item.name
-            button.id = 'buttons'
-        
-        let divText = document.createElement('div')
-            divText.classList.add('content')
+        div.classList.add('users')
+    //Botão principal
+    let button = document.createElement('button')
+        button.type = 'button'
+        button.textContent = item.name
+        button.id = 'buttons'
+    //Div onde contém texto sobre cada usário
+    let divText = document.createElement('div')
+        divText.classList.add('content')
+    //Propriedades dos usuários
+    let name = document.createElement('p')
+        name.innerHTML = `Nome: ${item.name}`
+    let email = document.createElement('p')
+        email.innerHTML = `Email: ${item.email}`
+    let idade = document.createElement('p')
+        idade.innerHTML = `Idade: ${item.idade}`
+    let cidade = document.createElement('p')
+        cidade.innerHTML = `Cidade: ${item.cidade}`
+    //Div de operações crud
+    let divButtons = document.createElement('div')
+        divButtons.id = 'container-buttons'
+    //Operação de editar e exluir
+    let link_edit = document.createElement('a')
+        link_edit.href = '/edit-user'
 
-        let name = document.createElement('p')
-            name.innerHTML = `Nome: ${item.name}`
-        let email = document.createElement('p')
-            email.innerHTML = `Email: ${item.email}`
-        let idade = document.createElement('p')
-            idade.innerHTML = `Idade: ${item.idade}`
-        let cidade = document.createElement('p')
-            cidade.innerHTML = `Cidade: ${item.cidade}`
+    let link_delete = document.createElement('a')
+        link_delete.href = '/delete-user'
+    //Imagens de editar e exluir
+    let edit_img = document.createElement('img')
+        edit_img.src = 'edit_icon.svg'
+        edit_img.classList.add('images-crud')
 
-        
-/*         let img = document.createElement('img')
-            img.src = 'edit_icon.svg'
-            img.classList.add('images-crud') */
+    let delete_img = document.createElement('img')
+        delete_img.src = 'delete_icon.svg'
+        delete_img.classList.add('images-crud')
+        //Adicionando elementos detro de elementos
+        link_edit.appendChild(edit_img)
+        link_delete.appendChild(delete_img)
 
+        divButtons.appendChild(link_edit)
+        divButtons.appendChild(link_delete)
+        divText.appendChild(name)
+        divText.appendChild(email)
+        divText.appendChild(idade)
+        divText.appendChild(cidade)
 
-            divText.appendChild(name)
-            divText.appendChild(email)
-            divText.appendChild(idade)
-            divText.appendChild(cidade)
-        
-            div.appendChild(button)
-           /*  div.appendChild(img) */
-            div.appendChild(divText)
-        divContainer.appendChild(div)
+        divText.appendChild(divButtons)
+
+        div.appendChild(button)
+        div.appendChild(divText)
+    divContainer.appendChild(div)
 }
