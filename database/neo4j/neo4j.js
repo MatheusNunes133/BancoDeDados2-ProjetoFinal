2efd26e029d6ae6a8d5b01ebd0259872d69fe154
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-03-18 12:45:05 -0300

    feat: Adiciona funcoes de criar, deletar e criar relacionamento no neo4j

---
+++ database/neo4j/neo4j.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+const neo4j = require('neo4j-driver')
+
+const uri =  `neo4j://${process.env.NEO4J_HOST}:${process.env.NEO4J_PORT}`
+const driver = neo4j.driver(uri, neo4j.auth.basic(
+    process.env.NEO4J_USER, process.env.NEO4J_PASSWORD
+))
+
+const session = driver.session()
+
+
+//Adiciona usuário no neo4j
+async function addToNeo4j(name, email){
+    try {
+        let query = `create(:Pessoa{name: '${name}', email: '${email}'})`
+        await session.run(query)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Remove usuário do neo4j
+async function removeToNeo4j(email){
+    try {
+        let query = `match(p:Pessoa{email: '${email}'}) detach delete p`
+        await session.run(query)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//
+async function createRelationship(req, res){
+    const { firstEmail, secondEmail, relationshipType } = req.body
+    try {
+        const mongo = require('../mongodb/mongo')
+        let query = `match(p1:Pessoa{email: '${firstEmail}'})
+                    optional match(p2:Pessoa{email: '${secondEmail}'})
+                    create (p1)-[r:${relationshipType}]->(p2)`
+        await session.run(query)
+
+        let relation = `${relationshipType} -> ${firstEmail}`
+        await mongo.setRelationshipMongo(secondEmail, relation)
+        return res.status(200).send()
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = {
+    addToNeo4j,
+    removeToNeo4j,
+    createRelationship
+}
+
+
+
+
+
+
