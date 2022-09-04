2cdd0e2a1b561401a9a07552c2a305326d01ba0a
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-09-04 20:13:50 -0300

    feat: Adiciona teste da funcao de resgatar SVG do postgres

---
+++ test/unitTest-Postgres.spec.js
@@ -0,0 +1,18 @@
+const {getSVG } = require("../database/postgres/postgres")
+
+jest.mock("../database/mongodb/mongo",()=>{
+    return jest.fn()
+})
+
+describe("Testando funções do Postgres", ()=>{
+    test("Espero retornar uma string informado que deu certo restagar o svg", async ()=>{
+        let req = {
+            body:{
+                cidade: "Cajazeiras"
+            }
+        }
+
+        let result  = await getSVG(req)
+        expect(result).toBe("Sucesso ao resgatar SVG")
+    })
+})
